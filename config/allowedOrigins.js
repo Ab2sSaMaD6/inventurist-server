0ccdfac9de5cf1a437fa2f752e7a5202dc6dce8f
ABDESSAMAD EL HAMOUDI <ab2ssamad6@gmail.com>
2022-07-10 19:06:50 +0100

    fix CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['https://www.yoursite.com', 'http://127.0.0.0:5500', 'http://localhost:3000']
+const allowedOrigins = ['https://inventurist.web.app', 'http://127.0.0.0:5500', 'http://localhost:3000']
 
 module.exports = allowedOrigins
